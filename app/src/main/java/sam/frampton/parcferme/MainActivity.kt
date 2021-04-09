6d74a54940d0736345985a633558e072e401da71
Sam Frampton <79669218+samframpton@users.noreply.github.com>
2021-04-09 14:24:38 +0100

    Add top level fragments and navigation
    - Add empty fragment for each top level destination
    
    - Add fragment container with bottom navigation to main activity
    
    - Navigate between the fragments using navigation library nav graph
    
    - Add icons for bottom navigation view
    
    - Add dependencies for navigation library with safe args plugin

--- app/src/main/java/sam/frampton/parcferme/MainActivity.kt
+++ app/src/main/java/sam/frampton/parcferme/MainActivity.kt
@@ -1,11 +1,20 @@
 package sam.frampton.parcferme
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavView.setupWithNavController(navController)
     }
 }
