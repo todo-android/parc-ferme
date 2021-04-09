5b711e23cc01f8912d8c4b2af46f95558b260b98
Sam Frampton <79669218+samframpton@users.noreply.github.com>
2021-04-09 15:48:44 +0100

    Implement driver list fragment recycler view
    - Update fragment layout to include recycler view with season spinner
    
    - Add a basic driver list item layout
    
    - Add a driver list recycler view adapter
    
    - Update fragment to implement all of the above

---
+++ app/src/main/java/sam/frampton/parcferme/adapters/DriverAdapter.kt
@@ -0,0 +1,46 @@
+package sam.frampton.parcferme.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import sam.frampton.parcferme.data.Driver
+import sam.frampton.parcferme.databinding.ListItemDriverBinding
+
+class DriverAdapter(private val onClick: (Driver) -> Unit) :
+    ListAdapter<Driver, DriverAdapter.ViewHolder>(DiffCallback) {
+
+    inner class ViewHolder(private val binding: ListItemDriverBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        private var driver: Driver? = null
+
+        init {
+            binding.root.setOnClickListener { driver?.let { onClick(it) } }
+        }
+
+        fun bind(driver: Driver) {
+            this.driver = driver
+            binding.driver = driver
+        }
+    }
+
+    private object DiffCallback : DiffUtil.ItemCallback<Driver>() {
+        override fun areItemsTheSame(oldItem: Driver, newItem: Driver): Boolean {
+            return oldItem.driverId == newItem.driverId
+        }
+
+        override fun areContentsTheSame(oldItem: Driver, newItem: Driver): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ListItemDriverBinding.inflate(LayoutInflater.from(parent.context))
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
