649d5c31c73a35cf04a1132cc4a27a472d3fedbc
Sam Frampton <79669218+samframpton@users.noreply.github.com>
2021-04-13 09:46:45 +0100

    Add RoomDatabase implementation

---
+++ app/src/main/java/sam/frampton/parcferme/database/AppDatabase.kt
@@ -0,0 +1,57 @@
+package sam.frampton.parcferme.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import sam.frampton.parcferme.database.daos.*
+import sam.frampton.parcferme.database.entities.*
+
+@Database(
+    entities = [
+        SeasonEntity::class,
+        DriverEntity::class,
+        SeasonDriverCrossRef::class,
+        ConstructorEntity::class,
+        SeasonConstructorCrossRef::class,
+        RaceEntity::class,
+        CircuitEntity::class,
+        RaceResultEntity::class,
+        QualifyingResultEntity::class,
+        DriverStandingEntity::class,
+        ConstructorStandingEntity::class,
+        DriverStandingConstructorCrossRef::class
+    ],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun seasonDao(): SeasonDao
+
+    abstract fun driverDao(): DriverDao
+
+    abstract fun constructorDao(): ConstructorDao
+
+    abstract fun raceDao(): RaceDao
+
+    abstract fun resultDao(): ResultDao
+
+    abstract fun standingDao(): StandingDao
+
+    companion object {
+        private const val DATABASE_NAME = "parcferme-db"
+
+        @Volatile
+        private var instance: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            return instance ?: synchronized(this) {
+                instance ?: Room.databaseBuilder(
+                    context, AppDatabase::class.java, DATABASE_NAME
+                )
+                    .build().also { instance = it }
+            }
+        }
+    }
+}
