7c6afaafb87badf8be678adc633c0863ffdd739e
Sam Frampton <79669218+samframpton@users.noreply.github.com>
2021-04-15 15:29:33 +0100

    Handle action bar up navigation

--- app/src/main/java/sam/frampton/parcferme/MainActivity.kt
+++ app/src/main/java/sam/frampton/parcferme/MainActivity.kt
@@ -2,21 +2,26 @@ package sam.frampton.parcferme
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        val appBarConfiguration = AppBarConfiguration(
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.driverListFragment,
                 R.id.constructorListFragment,
@@ -28,4 +33,7 @@ class MainActivity : AppCompatActivity() {
         val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         bottomNavView.setupWithNavController(navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean =
+        navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
 }
